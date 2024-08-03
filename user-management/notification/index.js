9ff9ac50a5e80c219d62b5694b4af4a26b8b888c
Muhammad Nawlo <nawlomuhammadit@gmail.com>
2024-08-03 03:48:03 +0300

    removing secret

--- user-management/notification/index.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase-admin';
-import firebaseConfig from './firebase-config.json';
-
-firebase.initializeApp({
-    credential: firebase.credential.cert(firebaseConfig)
-});
-export default firebase
-
